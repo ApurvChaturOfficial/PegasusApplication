b9b514340838caeeb845dbf6b5a67c4036649073
ApurvChaturOfficial <apurvchaturofficial@gmail.com>
2024-11-22 07:32:28 +0530

    backend base added

--- site/frontend/src/aConnection/cAPIConnection/index.ts
+++ site/frontend/src/aConnection/cAPIConnection/index.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiConnection = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_ENVIRONMENT === "Production" ? "https://pegasus-backend.onrender.com/api/v1/" : "http://localhost:8080/api/v1/",
+    baseUrl: import.meta.env.VITE_ENVIRONMENT === "Production" ? "https://pegasus-backend-f4bi.onrender.com/api/v1/" : "http://localhost:8080/api/v1/",
     credentials: "include"
   }),
   tagTypes: [
